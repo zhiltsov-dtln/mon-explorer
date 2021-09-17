b45dae6890dd28bab195cb55d6c251ec2cd46f85
Django User <monitoring@dtln.ru>
2021-09-17 17:39:14 +0300

    Initial setup with poetry

--- mcs/urls.py
+++ mcs/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from authtest import views
+#from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', views.test),
+    path('login/', views.login),
+    path('oidc/', include('mozilla_django_oidc.urls')),
 ]
